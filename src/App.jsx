5fc20465ddec374124d33976c9d6a67957fb72bb
Sammuel Kodzogan LEKPOR <samuellekpor@gmail.com>
2024-08-25 22:15:59 +0000

    Reorganized the code

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,12 @@
 import { useState } from "react";
+import HomePage from "./components/HomePage";
+import Header from "./components/Header";
 function App() {
   return (
-    <div className="flex flex-col p-4 max-w-[1000px] mx-auto w-full">
+    <div className="flex flex-col max-w-[1000px] mx-auto w-full">
       <section className="min-h-screen flex flex-col">
-        <header className="flex items-center justify-between gap-4">
-          <h1>
-            Free<span className="text-blue-400">Scribe</span>
-          </h1>
-          <button className="flex items-center gap-2">
-            <p>New</p>
-            <i className="fa-solid fa-plus"></i>
-          </button>
-        </header>
-        <main className="flex-1"></main>
+        <Header />
+        <HomePage />
       </section>
       <h1 className="text-3xl font-bold text-red-800">hello</h1>
       <footer></footer>
